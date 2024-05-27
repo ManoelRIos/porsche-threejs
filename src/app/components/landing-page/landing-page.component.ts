d72bc6f4c943922926e1925aedcc826f85f81698
Manoel Rios <manoelriosneto@gmail.com>
2024-05-27 17:01:08 -0300

    Adicionando s light

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -14,38 +14,92 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 export class LandingPageComponent {
   width = window.innerWidth;
   height = window.innerHeight;
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const camera = new THREE.PerspectiveCamera(
       45,
       this.width / this.height,
-      1,
+      0.1,
       1000
-    );
-    camera.position.set(4, 5, 11);
-    camera.lookAt(0, 0, 0);
-    const scene = new THREE.Scene();
+    );    
 
+    
+    const scene = new THREE.Scene();
     // Adiciona iluminação
-    const pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(5, 5, 5);
-    const ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(pointLight, ambientLight);
+    const pointLight = new THREE.PointLight(0x404040);
+    pointLight.position.set(5, 10, 5);
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    // scene.add(pointLight, ambientLight);
+    // scene.fog = new THREE.Fog( 0xffffff, 2, 1.1);      
+    const spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(0, 30, -3);
+    spotLight.intensity = 1.2
+    spotLight.angle = 0.45
+    spotLight.penumbra = 0.3
+    spotLight.castShadow = true
+    spotLight.shadow.mapSize.width = 1024
+    spotLight.shadow.mapSize.height = 1024
+    spotLight.shadow.camera.near = 5
+    spotLight.shadow.camera.far = 15
+    spotLight.shadow.focus = 1
+    scene.add(spotLight);
+
+    var light = new THREE.DirectionalLight(0xffffff, 1); // Luz branca com intensidade 1
+    light.position.set(0, 10, 10); // Posição da luz
+    // scene.add(light);
+
+    const directionalLightHelper = new THREE.DirectionalLightHelper(light, 10);
     const lightHelper = new THREE.PointLightHelper(pointLight);
     const gridHelper = new THREE.GridHelper(200, 50);
-    // const axesHelper = new THREE.AxesHelper(100);
-    scene.add(lightHelper, gridHelper /* axesHelper */);
+    const axesHelper = new THREE.AxesHelper(100);    
+    const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    scene.add(lightHelper, gridHelper, axesHelper, directionalLightHelper,spotLightHelper);
+
+
+
+    // Sombras
+    const groundGeometry = new THREE.PlaneGeometry(32, 32, 32, 32);
+    groundGeometry.rotateX(-Math.PI / 2);
+    const groundTexture = new THREE.TextureLoader().load(
+      '../../../assets/floor/textures/asphalt_02_diff_4k.jpg'
+    );
+    const groundTexture3d = new THREE.TextureLoader().load(
+      '../../../assets/floor/textures/asphalt_02_disp_4k.png'
+    );    
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;    
+    groundTexture.anisotropy = 16;
+    const groundMaterial = new THREE.MeshStandardMaterial({
+      map: groundTexture,
+      normalMap: groundTexture3d,
+    });
+
+    const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+    groundMesh.position.set(0, 0.01, -2.5);
+    // scene.add(groundMesh);
+
+    const planeGeo = new THREE.PlaneGeometry(100,100)
+    const planeMat = new THREE.MeshPhongMaterial({color: 0xffffff, dithering: true})    
+    const plane = new THREE.Mesh(planeGeo, planeMat)
+    plane.rotation.x = -0.5 * Math.PI
+    scene.add(plane)
+    plane.receiveShadow = true
+
 
     //Adiciona controles 3D
     const controls = new OrbitControls(camera, document.body);
+    // controls.enableZoom = false
+    // controls.enablePan = false
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    const canvas = document.getElementById('threejs-canvas');
-    canvas?.appendChild(renderer.domElement);
-    renderer.setClearColor(0x111827);
-    renderer.setSize(this.width, this.height);
-    renderer.setAnimationLoop(animation);
+    renderer.outputColorSpace = THREE.SRGBColorSpace;    
+    renderer.setClearColor(0x18181b);    
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true;    
 
-    renderer.setSize(this.width / 2, this.height / 2);
+    const canvas = document.getElementById('threejs-canvas');
+    const widthCanva = canvas!.offsetWidth;
+    const heightCanva = canvas!.offsetHeight;
+    renderer.setSize(widthCanva, heightCanva);
+    canvas?.appendChild(renderer.domElement);
 
     const loader = new GLTFLoader();
     const rgbeLoader = new RGBELoader();
@@ -54,7 +108,7 @@ export class LandingPageComponent {
     renderer.toneMappingExposure = 1; // Adjust exposure as needed
 
     rgbeLoader.load(
-      '../../../assets/MRHDRI/MR_INT-005_WhiteNeons_NAD.hdr',
+      '../../../assets/MRHDRI/MR_INT-006_LoftIndustrialWindow_Griffintown.hdr',
       (texture) => {
         texture.mapping = THREE.EquirectangularReflectionMapping;
         scene.environment = texture;
@@ -62,25 +116,29 @@ export class LandingPageComponent {
     );
 
     let objectCar: any;
-    loader.load('../../../assets/porscheTurbo/scene.gltf', (gltf) => {
+    loader.load('../../../assets/porsche/scene.gltf', (gltf) => {
       console.log(gltf);
       const mesh = gltf.scene;
       objectCar = mesh;
-      mesh.position.set(0, 0, 0);
-      // const carTexture = new THREE.TextureLoader().load(
-      //   '../../../assets/porsche/textures/coat_baseColor.png'
-      // );
+
+      mesh.castShadow = true
+      mesh.receiveShadow = true
+      mesh.scale.set(1.5, 2.25, 1.5);
+      mesh.position.set(0, 0, -3);
+      renderer.setAnimationLoop(animation);
 
       scene.add(mesh);
       renderer.render(gltf.scenes[0], camera);
     });
 
+    camera.position.set(15, 10, 15);
+    camera.lookAt(scene.position);
+
     // animation
     function animation(time: number) {
-      /*     objectCar.rotation.x += 0.0001;
-      objectCar.rotation.y += 0.0005;
-      objectCar.rotation.z += 0.001; */
-
+      // objectCar.rotation.x += 0.0001;
+      objectCar.rotation.y -= 0.0005;
+      // objectCar.rotation.z += 0.001;
       renderer.render(scene, camera);
     }
   }
